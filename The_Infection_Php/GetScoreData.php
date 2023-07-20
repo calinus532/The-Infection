f1ddc805b05aa25dc0352b24d5bde44cf9139c7c
calinus532 <76541681+calinus532@users.noreply.github.com>
2023-07-20 05:02:44 +0300

    Update GetScoreData.php

--- The_Infection_Php/GetScoreData.php
+++ The_Infection_Php/GetScoreData.php
@@ -2,12 +2,12 @@
 include('Connection.php'); 
 
 $nickname = $_POST['currentNickName'];
-$sql = "SELECT NickName, Level, Time, Score from scor WHERE NickName LIKE '".$nickname."'"; 
+$sql = "SELECT NickName, Level, Time, Score, MazeType from scor WHERE NickName LIKE '".$nickname."'"; 
 $result = mysqli_query($connect, $sql); 
 
 if(mysqli_num_rows($result) > 0) { 
     while($row = mysqli_fetch_assoc($result)) { 
-        echo $row['Level'] . "/" . $row['Time'] . "/" . $row['Score']; 
+        echo $row['Level'] . "/" . $row['Time'] . "/" . $row['Score'] . "/" . $row['MazeType']; 
     } 
 } 
 
